cf180bf7935159d7d168b581f30f2174c0bd92b9
Aswin Asok <aswinasokofficial@gmail.com>
2023-02-15 00:39:31 +0530

    develop:api-integration-resetpass-done

--- src/Pages/Authentication/ResetPassword/ResetPassword.tsx
+++ src/Pages/Authentication/ResetPassword/ResetPassword.tsx
@@ -1,13 +1,79 @@
 import ReactPasswordChecklist from "react-password-checklist"
+import { CountdownCircleTimer } from "react-countdown-circle-timer"
 import styles from "./ResetPassword.module.css"
 
 import { useState } from "react"
 import { Button } from "@chakra-ui/react"
+import axios from "axios"
+
+import { Alert, AlertIcon, AlertDescription } from "@chakra-ui/react"
 
 const ResetPassword = () => {
   const [email, setEmail] = useState("")
+  const [secret, setSecret] = useState("")
   const [password, setPassword] = useState("")
   const [passwordAgain, setPasswordAgain] = useState("")
+
+  const [status, setStatus] = useState(1)
+  const [statusMessage, setStatusMessage] = useState("")
+
+  const [resent, setResent] = useState(false)
+  const [timer, setTimer] = useState(false)
+
+  const initResetPass = () => {
+    axios
+      .post(`${import.meta.env.VITE_APP_BACKEND_URL}/init_reset_pass`, {
+        email: email,
+      })
+      .then(function (response) {
+        setStatus(response.status)
+        setStatusMessage("Reset Key Successfully Sent!")
+
+        setTimer(true)
+      })
+      .catch(function (error) {
+        setStatus(error.response.status)
+        setStatusMessage("You have Entered an Invalid Email Address")
+      })
+  }
+
+  const initResend = () => {
+    axios
+      .post(`${import.meta.env.VITE_APP_BACKEND_URL}/init_resend`, {
+        email: email,
+      })
+      .then(function (response) {
+        setStatus(response.status)
+        setStatusMessage("Reset Key Successfully Sent Again!")
+
+        setTimer(true)
+      })
+      .catch(function (error) {
+        console.log(error)
+        setStatus(error.response.status)
+        setStatusMessage("You have Entered an Invalid Email Address")
+      })
+  }
+
+  const resetPass = () => {
+    axios
+      .post(`${import.meta.env.VITE_APP_BACKEND_URL}/reset_pass`, {
+        email: email,
+        secret: secret,
+        new_password: passwordAgain,
+      })
+      .then(function (response) {
+        setStatus(response.status)
+        setStatusMessage("Password Reset Is Successful!")
+      })
+      .catch(function (error) {
+        setStatus(error.response.status)
+        setStatusMessage(
+          "Password Reset Failed, Make Sure you Entered Correct Token!"
+        )
+      })
+  }
+
   return (
     <div className={styles.main_container}>
       <div className={styles.first_view_container}>
@@ -21,29 +87,96 @@ const ResetPassword = () => {
             <p className={styles.fv_heading}>Sent Password Reset Key</p>
             <p className={styles.fv_tagline}>
               Enter in your email address to receive the secret key for
-              resetting the password.
+              resetting your account password.
             </p>
           </div>
           <div>
             <p className={styles.fv_input_field_label}>Email Address</p>
-            <input type="email" className={styles.input_field} />
+            <input
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value)
+              }}
+              type="email"
+              className={styles.input_field}
+            />
 
-            <Button marginTop={"1rem"} colorScheme="linkedin" size="md">
-              Sent Reset Key
-            </Button>
+            {secret.length === 0 &&
+              status &&
+              (status === 200 || status === 400) && (
+                <Alert
+                  marginTop="1rem"
+                  marginBottom="1rem"
+                  variant="left-accent"
+                  width="80%"
+                  status={status === 400 ? "error" : "success"}
+                >
+                  <AlertIcon />
+                  <div>
+                    <AlertDescription>{statusMessage}</AlertDescription>
+                  </div>
+                </Alert>
+              )}
+            {!resent && !timer && (
+              <Button
+                onClick={() => {
+                  initResetPass()
+                }}
+                marginTop={"1rem"}
+                colorScheme="linkedin"
+                size="md"
+              >
+                Sent Reset Key
+              </Button>
+            )}
+            {resent && !timer && (
+              <Button
+                onClick={() => {
+                  initResend()
+                }}
+                marginTop={"1rem"}
+                colorScheme="linkedin"
+                size="md"
+              >
+                Resent Secret Key
+              </Button>
+            )}
+            {timer && (
+              <div className={styles.timer}>
+                <CountdownCircleTimer
+                  strokeWidth={6}
+                  size={50}
+                  isPlaying
+                  duration={10}
+                  colors={"#2f8cc9"}
+                  onComplete={() => {
+                    setResent(true)
+                    setTimer(false)
+                    setStatus(1)
+                  }}
+                >
+                  {({ remainingTime }) => remainingTime}
+                </CountdownCircleTimer>
+                <span>Seconds Left</span>
+              </div>
+            )}
           </div>
         </div>
         <div className={styles.form_container}>
           <div className={styles.fv_texts}>
             <p className={styles.fv_heading}>Reset Your Account Password</p>
             <p className={styles.fv_tagline}>
-              Hi There, Enter in your email and password to login to the your
-              product pack dashboard.
+              Enter in the secret key your received from the email we just sent
+              you.
             </p>
           </div>
           <div className={styles.fv_input_form}>
             <p className={styles.fv_input_field_label}>Enter Secret Key</p>
-            <input type="text" className={styles.input_field} />
+            <input
+              onChange={(e) => setSecret(e.target.value)}
+              type="text"
+              className={styles.input_field}
+            />
             <p className={styles.fv_input_field_label}>Password</p>
             <input
               type="password"
@@ -73,14 +206,30 @@ const ResetPassword = () => {
                 valueAgain={passwordAgain}
               />
             </div>
+            {secret && status && (status === 200 || status === 400) && (
+              <Alert
+                marginTop="1rem"
+                marginBottom="1rem"
+                variant="left-accent"
+                width="80%"
+                status={status === 400 ? "error" : "success"}
+              >
+                <AlertIcon />
+                <div>
+                  <AlertDescription>{statusMessage}</AlertDescription>
+                </div>
+              </Alert>
+            )}
           </div>
-          <Button colorScheme="linkedin" size="md">
+          <Button
+            onClick={() => {
+              resetPass()
+            }}
+            colorScheme="linkedin"
+            size="md"
+          >
             Set Password
           </Button>
-          <div className={styles.secondary_options}>
-            <p className={styles.so_text}>Forgot Password?</p>
-            <p className={styles.so_text}>Already have an Account?</p>
-          </div>
         </div>
       </div>
     </div>
